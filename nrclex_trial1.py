c2f0ad7835c677b2ffba4752b46b4a9f20dca149
Chong Pok Shun <pschong261199@hotmail.comgit>
2022-04-23 01:50:22 +0800

    working for rock, need some adjustments

---
+++ nrclex_trial1.py
@@ -0,0 +1,197 @@
+# Code with reference to https://github.com/metalcorebear/NRCLex
+from operator import length_hint
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import pandas as pd
+import re
+# from nrclex import NRCLeximport as nltk
+# nltk.download('punkt')
+# nltk.download('wordnet')
+from textblob import TextBlob, Word, Blobber
+
+from textblob import TextBlob
+from collections import Counter
+from json import load
+
+
+def __build_word_affect__(self):
+    '''
+    Instantiates the following attributes:
+        affect_list
+        affect_dict
+        raw_emotion_scores
+        affect_frequencies
+    '''
+    affect_list = []
+    affect_dict = dict()
+    affect_frequencies = Counter()
+    lexicon_keys = self.__lexicon__.keys()
+    for word in self.words:
+        if word in lexicon_keys:
+            affect_list.extend(self.__lexicon__[word])
+            affect_dict.update({word: self.__lexicon__[word]})
+    for word in affect_list:
+        affect_frequencies[word] += 1
+    sum_values = sum(affect_frequencies.values())
+    affect_percent = {'fear': 0.0, 'anger': 0.0, 'anticipation': 0.0, 'trust': 0.0, 'surprise': 0.0, 'positive': 0.0,
+                      'negative': 0.0, 'sadness': 0.0, 'disgust': 0.0, 'joy': 0.0}
+    for key in affect_frequencies.keys():
+        affect_percent.update(
+            {key: float(affect_frequencies[key]) / float(sum_values)})
+    self.affect_list = affect_list
+    self.affect_dict = affect_dict
+    self.raw_emotion_scores = dict(affect_frequencies)
+    self.affect_frequencies = affect_percent
+
+
+def top_emotions(self):
+    '''
+    top_emotions becomes a list of (emotion: str, score: float) with the highest score associated to the input text.
+    '''
+    emo_dict = self.affect_frequencies
+    max_value = max(emo_dict.values())
+    top_emotions = []
+    for key in emo_dict.keys():
+        if emo_dict[key] == max_value:
+            top_emotions.append((key, max_value))
+    self.top_emotions = top_emotions
+
+
+class NRCLex:
+    """Lexicon source is (C) 2016 National Research Council Canada (NRC) and library is for research purposes only.  Source: http://sentiment.nrc.ca/lexicons-for-research/"""
+
+    def __init__(self, lexicon_file='nrc_en.json'):
+        with open(lexicon_file, 'r') as json_file:
+            self.__lexicon__ = load(json_file)
+
+    def load_token_list(self, token_list):
+        '''
+        Load an already tokenized text (as a list of tokens) into the NRCLex object.
+        This is for when you want to use NRCLex with a text that you prefer to tokenize and/or lemmatize yourself.
+        Parameters:
+            token_list (list): a list of utf-8 strings.
+        Returns:
+            No return
+        '''
+        self.text = ""
+        self.words = token_list
+        self.sentences = []
+        __build_word_affect__(self)
+        top_emotions(self)
+
+    def load_raw_text(self, text):
+        '''
+        Load a string into the NRCLex object for tokenization and lemmatization with TextBlob.
+        Parameters:
+            text (str): a utf-8 string.
+        Returns:
+            No return
+        '''
+        self.text = text
+        blob = TextBlob(self.text)
+        self.words = [w.lemmatize() for w in blob.words]
+        self.sentences = list(blob.sentences)
+        __build_word_affect__(self)
+        top_emotions(self)
+
+
+def makeRadarGraph(key, values):
+
+    # the 10 emotions selected
+    header = ["anger", "disgust", "fear", "negative", "sadness",
+              "anticipation", "positive", "trust", "surprise", "joy"]
+
+    # Split the circle into even parts and save the angles
+    # so we know where to put each axis.
+    angles = np.linspace(0, 2 * np.pi, 10, endpoint=False).tolist()
+
+    # The plot is a circle, so we need to "complete the loop"
+    # and append the start value to the end.
+    values += values[:1]
+    angles += angles[:1]
+
+    # ax = plt.subplot(polar=True)
+    fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
+
+    # Draw the outline of our data.
+    ax.plot(angles, values, color='red', linewidth=1)
+    # ax.plot(angles, values1, color='orange', linewidth=1)
+    # Fill it in.
+    ax.fill(angles, values, color='red', alpha=0.25, )
+    # ax.fill(angles, values1, color='orange', alpha=0.25)
+
+    # Fix axis to go in the right order and start at 12 o'clock.
+    ax.set_theta_offset(np.pi / 2)
+    ax.set_theta_direction(-1)
+
+    # Draw axis lines for each angle and label.
+    ax.set_thetagrids(np.degrees(angles[:-1]), header)
+
+    # Go through labels and adjust alignment based on where
+    # it is in the circle.
+    for label, angle in zip(ax.get_xticklabels(), angles):
+        if angle in (0, np.pi):
+            label.set_horizontalalignment('center')
+        elif 0 < angle < np.pi:
+            label.set_horizontalalignment('left')
+        else:
+            label.set_horizontalalignment('right')
+    plt.title(key, size=20)
+    plt.legend(["lyrics", ""])
+    plt.show()
+
+
+"""START OF CODE"""
+
+text_object = NRCLex(lexicon_file='nrc_en.json')
+
+
+# to read the songs
+with open("selectedsongs.txt", 'r') as f:
+    lyrics = f.read()
+    datas = lyrics.split("\n\n")
+    # print("Data: ", datas) # lyrics in text with \n
+    data = ".".join(datas)
+    datas = data.split("\n \n")
+    data = ".".join(datas)
+    datas = data.split(".")
+    data = datas
+
+newdata = []
+for lines in data:
+    newline = lines.replace("\n", " ")
+    newdata.append(newline)
+# print("New Data: ", newdata)  # filtered \n out
+rows_list = []
+title_list = []
+for num in range(len(newdata)):
+    # split even index into title_list
+    # split odd index to rows_list, which is the lyrics
+    if(num % 2 == 0):
+        if(newdata[num] != ''):
+            title_list.append(newdata[num])
+    else:
+        text_object.load_raw_text(newdata[num])
+        rows_list.append(text_object.affect_frequencies)
+# print(title_list) # title list
+print("Row list: ", rows_list)  # the different data of different songs
+
+
+"""RADAR GRAPH PLOTTING HERE"""
+
+df = pd.DataFrame(rows_list, index=title_list[:])
+
+# print("DF HERE: ", df)
+
+# the 10 emotions selected
+header = ["anger", "disgust", "fear", "negative", "sadness",
+          "anticipation", "positive", "trust", "surprise", "joy"]
+# iloc is the [row][col]
+dictionary = {}
+for rowindex in range(len(df.iloc[:])):
+    dictionary[title_list[rowindex]] = df.iloc[rowindex][:].tolist()
+
+print("the dictionary: ", dictionary)
+for key, value in dictionary.items():
+    makeRadarGraph(key, value)  # function call
--- webscraplyrics.py
+++ webscraplyrics.py
@@ -13,8 +13,8 @@ from bs4 import BeautifulSoup
 def findlyrics(dictionary):
     songrequest_dic = {}
     for key, value in dictionary.items():
-        songrequest = value
-        artist = key
+        songrequest = key
+        artist = value
 
         # search for the song using track name and artist name
         edited_songrequest = songrequest.lower()
@@ -33,56 +33,74 @@ def findlyrics(dictionary):
         print("Results: ", results)
         if results.__contains__("Sorry, we have no"):
             print("error")
-            break
-        # scraping with regex
-        initialgrab = re.findall(">[\w\s,!'â€~]+", results)
-        print("Initialgrab: ", initialgrab)
-        finalgrab = []
-        for clause in initialgrab:
-            clause = re.sub("[>\nâ€~]", "", clause)
-            print("Clause: ", clause)
-            if clause != "":
-                finalgrab.append(clause)
-        lyrics = ""
-        for line in finalgrab:
-            lyrics += line + "\n "
-        songrequest_dic[f"{edited_songrequest} by {edited_artist}"] = lyrics
+        else:
+            # scraping with regex
+            initialgrab = re.findall(">[\w\s,!'â€~]+", results)
+            print("Initialgrab: ", initialgrab)
+            finalgrab = []
+            for clause in initialgrab:
+                clause = re.sub("[>\nâ€~]", "", clause)
+                print("Clause: ", clause)
+                if clause != "":
+                    finalgrab.append(clause)
+            lyrics = ""
+            for line in finalgrab:
+                lyrics += line + "\n "
+            if lyrics != "":
+                songrequest_dic[f"{edited_songrequest} by {edited_artist}"] = lyrics
 
         # ending sequence
     # store lyrics in txt file
-    with open('selectedsongs.txt', 'w') as f:
+    with open('selectedsongsrock.txt', 'w') as f:
+        indexing = 0
         for key, value in songrequest_dic.items():
             key = re.sub("-", " ", key)
-            f.write(key.title() + "\n")
+            f.write(f"__Title__ " + key.title() + "\n")
             f.write('\n')
             f.write(value)
             f.write('\n')
-    print("Ended boo")
+            indexing += 1
+    print("INDEX: ", indexing)
     return songrequest_dic
 
 
-dictionary = {"paramore": "misery business",
-              "My Chemical Romance": "Helena",
-              "My Chemical romance": "I'm not okay",
-              "Blink 182": "I miss you",
-              "The Red JumpSuit Apparatus": "Face Down",
-              "Fall out Boy": "Thnks fr th Mmrs",
-              "Good charlotte": "the anthem",
-              "Linkin Park": "numb",
-              "YellowCard": "Ocean Avenue",
-              "Mayday parade": "Miserable at best",
-              "Sleeping with sirens": "If you can't hang",
-              "blink 182": "First Date",
-              "Sum41": "In too deep",
-              "bring me the horizon": "throne",
-              "Bullet For my valentine": "Tears Don't Fall",
-              "Mayday Parade": "Jamie All Over",
-              "my Chemical Romance": "Welcome to the Black Parade",
-              "Pierce the Veil": "King for a day",
-              "All Time Low": "Therapy",
-              "Simple Plan": "Perfect",
-              "We the Kings": "Skyway Avenue",
-              "The Killers": "Mr Brightside"
-              }
+with open("rock.txt", 'r') as f:
+    titles = f.read()
+    titles = re.sub("\d+\\t", "", titles)
+    filter = titles.split("\n")
 
-findlyrics(dictionary)
+    dict = {}
+    for wordtgt in filter:
+        temp = wordtgt.split("\t")
+        print(temp)
+        try:
+            dict[temp[1]] = temp[0]
+        except:
+            print("skip this song")
+
+print(dict)
+# dictionary = {"paramore": "misery business",
+#               "My Chemical Romance": "Helena",
+#               "My Chemical romance": "I'm not okay",
+#               "Blink 182": "I miss you",
+#               "The Red JumpSuit Apparatus": "Face Down",
+#               "Fall out Boy": "Thnks fr th Mmrs",
+#               "Good charlotte": "the anthem",
+#               "Linkin Park": "numb",
+#               "YellowCard": "Ocean Avenue",
+#               "Mayday parade": "Miserable at best",
+#               "Sleeping with sirens": "If you can't hang",
+#               "blink 182": "First Date",
+#               "Sum41": "In too deep",
+#               "bring me the horizon": "throne",
+#               "Bullet For my valentine": "Tears Don't Fall",
+#               "Mayday Parade": "Jamie All Over",
+#               "my Chemical Romance": "Welcome to the Black Parade",
+#               "Pierce the Veil": "King for a day",
+#               "All Time Low": "Therapy",
+#               "Simple Plan": "Perfect",
+#               "We the Kings": "Skyway Avenue",
+#               "The Killers": "Mr Brightside"
+#               }
+
+findlyrics(dict)
